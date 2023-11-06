743e755320afe6f5a0d64c65c5daf66ba66f7aaa
mostak9 <mostak.ahm263@gmail.com>
2023-11-07 00:08:50 +0600

    created allBooks api

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6nmlwzx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -25,11 +21,18 @@ const client = new MongoClient(uri, {
   }
 });
 
+const bookCollections = client.db('libraryManager').collection('allBooksCollection');
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    app.get('/api/v1/allBooks', async(req, res)  => {
+        const cursor = bookCollections.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
 
 
 

3fdfc23855617946f82a6ebe13166e7a913fb6fb
mostak9 <mostak.ahm263@gmail.com>
2023-11-08 19:49:18 +0600

    created a cookie on the serever

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -8,6 +10,7 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6nmlwzx.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -30,6 +33,19 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+
+    // auth related api
+    // auth related api
+    app.post('/api/v1/jwt', async (req, res)  => {
+      const user = req.body;
+      console.log(user);
+      
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send(token);
+    })
+
+
+    // service related api 
     app.get("/api/v1/allBooks", async (req, res) => {
       const query = req.query;
       const cursor = query?.category
@@ -42,11 +58,11 @@ async function run() {
 
     app.get("/api/v1/allBooks/:id", async (req, res) => {
       const id = req.params.id;
-      console.log(id);
+      // console.log(id);
       const queryTxt = req?.query?.read;
       const query = { _id: new ObjectId(id) };
       const options = {
-        projection: { _id: 1, title: 1, link: 1, author: 1 },
+        projection: { _id: 1, title: 1, link: 1, author: 1, quantity: 1 },
       };
       const book = queryTxt
         ? await bookCollections.findOne(query, options)
@@ -99,7 +115,7 @@ async function run() {
     // borrowed books api
     app.post('/api/v1/borrowBook', async(req, res) => {
       const data = req.body;
-      console.log(data);
+      // console.log(data);
       const result = await borrowCollection.insertOne(data);
       res.send(result);
     });
@@ -107,6 +123,13 @@ async function run() {
     app.get('/api/v1/borrowBook', async(req, res) => {
       const query = req.query.email;
       const result = await borrowCollection.find({email: query}).toArray();
+      res.send(result);
+    })
+    app.delete('/api/v1/deleteBorrowedBook/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log('id from delete', id);
+      const query = {_id: new ObjectId(id)};
+      const result = await borrowCollection.deleteOne()
       res.send(result);
     })
 

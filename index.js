b9174cd83706772b7685f24fde9b1ee0e43ac4c4
mostak9 <mostak.ahm263@gmail.com>
2023-11-08 15:28:48 +0600

    added api to borrow book and update quantiy

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-const cors = require('cors');
+const cors = require("cors");
 const app = express();
-
 
 // middleware
 app.use(cors());
@@ -18,39 +17,98 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-const bookCollections = client.db('libraryManager').collection('allBooksCollection');
+const bookCollections = client
+  .db("libraryManager")
+  .collection("allBooksCollection");
+const borrowCollection = client.db('libraryManager').collection('borrowedBooksCollection');
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    app.get('/api/v1/allBooks', async(req, res)  => {
-        const query = req.query;
-        console.log(query);
-        const cursor = query?.category ? bookCollections.find({category: query.category}) : bookCollections.find();
-        
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+    app.get("/api/v1/allBooks", async (req, res) => {
+      const query = req.query;
+      const cursor = query?.category
+        ? bookCollections.find({ category: query.category })
+        : bookCollections.find();
 
-    app.get('/api/v1/allBooks/:id', async (req, res) => {
-      const id = parseInt(req.params.id);
-      const query = {id: id}
-      const book = await bookCollections.findOne(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/api/v1/allBooks/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const queryTxt = req?.query?.read;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { _id: 1, title: 1, link: 1, author: 1 },
+      };
+      const book = queryTxt
+        ? await bookCollections.findOne(query, options)
+        : await bookCollections.findOne(query);
       res.send(book);
+    });
+
+    app.post("/api/v1/addBook", async (req, res) => {
+      const book = req.body;
+      // console.log(book);
+      const result = await bookCollections.insertOne(book);
+      res.send(result);
+    });
+
+    app.put("/api/v1/updateBook/:id", async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          title: data.title,
+          author: data.author,
+          imageLink: data.imageLink,
+          quantity: data.quantity,
+          rating: data.rating,
+          category: data.category,
+          description: data.description,
+          pages: data.pages,
+          link: data.link,
+        },
+      };
+      const options = {upsert: true}
+      const result = await bookCollections.updateOne(filter,updateDoc,options);
+      res.send(result);
+    });
+
+    app.patch('/api/v1/updateQuantity/:id', async (req, res)  => {
+      const data = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          quantity: data.quantity,
+        }
+      }
+      const result = await bookCollections.updateOne(filter, updateDoc);
+      res.send(result);
     })
 
-
-
-
+    // borrowed books api
+    app.post('/api/v1/borrowBook', async(req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await borrowCollection.insertOne(data);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -58,10 +116,8 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("LIBRARY SERVER IS IN ONLINE");
+});
 
-
-app.get('/', (req, res)  => {
-    res.send('LIBRARY SERVER IS IN ONLINE');
-})
-
-app.listen(port, ()  => console.log(`listening on ${port}`));
+app.listen(port, () => console.log(`listening on ${port}`));
